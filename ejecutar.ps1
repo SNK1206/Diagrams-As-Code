4e930a1588321fc1f36c20fa25ac7f99f3c1cd65
Omar-G03 <omgujimenezpe@ittepic.edu.mx>
2026-05-30 19:25:12 -0700

    Implementacion de Ramas

--- ejecutar.ps1
+++ ejecutar.ps1
@@ -1,5 +1,8 @@
+$SDK = "$PSScriptRoot\javafx-sdk-21.0.9\lib"
+
 # === COMPILAR ===
-javac --module-path "C:\Users\bobal\Desktop\openjfx-26.0.1_windows-x64_bin-sdk\javafx-sdk-26.0.1\lib" --add-modules javafx.controls,javafx.fxml -cp src -d out (Get-ChildItem -Recurse -Filter "*.java" src | Select-Object -ExpandProperty FullName)
+New-Item -ItemType Directory -Force out | Out-Null
+javac --module-path $SDK --add-modules javafx.controls,javafx.fxml -cp src -d out (Get-ChildItem -Recurse -Filter "*.java" src | Select-Object -ExpandProperty FullName)
 
 # === EJECUTAR ===
-java --module-path "C:\Users\bobal\Desktop\openjfx-26.0.1_windows-x64_bin-sdk\javafx-sdk-26.0.1\lib" --add-modules javafx.controls,javafx.fxml -cp out com.diagramas.MainFX
+java --module-path $SDK --add-modules javafx.controls,javafx.fxml -cp out com.diagramas.MainFX
